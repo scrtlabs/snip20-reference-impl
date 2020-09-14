38dc858b94da3684fa73726211d3a98184d0b141
Reuven Podmazo <reuven@enigma.co>
2020-09-14 12:56:33 +0300

    ResponseStatus variants now resialize as snake_case

--- src/msg.rs
+++ src/msg.rs
@@ -111,6 +111,7 @@ pub struct CreateViewingKeyResponse {
 }
 
 #[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
 pub enum ResponseStatus {
     Success,
     Failure,
