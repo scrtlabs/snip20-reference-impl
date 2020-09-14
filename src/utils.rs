e855bd65cb84d61daedc63b72facae389e6e9f3a
Reuven Podmazo <reuven@enigma.co>
2020-09-14 14:58:05 +0300

    removed unused imports from utils.rs

--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,4 @@
 use bcrypt_pbkdf::bcrypt_pbkdf;
-use serde::export::Formatter;
-use std::fmt;
 use subtle::ConstantTimeEq;
 
 // 5 rounds == ~300M gas (doesn't work with query) - creation/validation takes ~1.5s
