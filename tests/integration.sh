a22bee4adcb75a8a016f19151f4d12abbb8723c5
Lior Bondarevsky <liorbond@gmail.com>
2023-03-26 19:37:01 +0300

    Check

--- tests/integration.sh
+++ tests/integration.sh
@@ -1666,7 +1666,7 @@ function measure_transfer_gas() {
 
     local transfer_message='{"transfer":{"recipient":"'"${ADDRESS[b]}"'","amount":"5"}}'
     local transfer_response
-    tx_hash="$(compute_execute "$contract_addr" "$transfer_message" ${FROM[a]} --gas 150000)"
+    tx_hash="$(compute_execute "$contract_addr" "$transfer_message" ${FROM[a]} --gas 1500000)"
     # Notice the `!` before the command - it is EXPECTED to fail.
     ! transfer_response="$(wait_for_compute_tx "$tx_hash" 'waiting for transfer from "a" to "b" to process')"
      ! transfer_response_not_compute="$(wait_for_tx "$tx_hash" 'waiting for transfer from "a" to "b" to process')"
@@ -1692,14 +1692,14 @@ function measure_transfer_with_decoys_gas() {
 
     local transfer_message='{"transfer":{"recipient":"'"${ADDRESS[b]}"'","amount":"5", "entropy":"TXlQYXN3b3JkMTIz", "decoys":["secret1kmgdagt5efcz2kku0ak9ezfgntg29g2vr88q0e","secret18vd8fpwxzck93qlwghaj6arh4p7c5n8978vsyg","secret1qqq8g6cjht0qfemed6nmrgjp8gajhnw3panxcf","secret1qqg9z5473z4f842c9nddc2942mj82lw8h7n5uf","secret1qqjmkf275wwaeevkkmqdk5xw2j7k5u5c8j7e9y"]}}'
     local transfer_response
-    tx_hash="$(compute_execute "$contract_addr" "$transfer_message" ${FROM[a]} --gas 150000)"
+    tx_hash="$(compute_execute "$contract_addr" "$transfer_message" ${FROM[a]} --gas 1500000)"
     # Notice the `!` before the command - it is EXPECTED to fail.
     ! transfer_response="$(wait_for_compute_tx "$tx_hash" 'waiting for transfer from "a" to "b" to process')"
     ! transfer_response_not_compute="$(wait_for_tx "$tx_hash" 'waiting for transfer from "a" to "b" to process')"
     
-    assert_ne "$(get_balance "$contract_addr" 'b')" $b_balance
-
     echo $transfer_response_not_compute
+    assert_ne "$(get_balance "$contract_addr" 'b')" $b_balance
+    
     # echo "$(extract_gas "$transfer_response")"
 }
 
